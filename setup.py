5ec81605c386e369a50abe9d499ea61cda1be357
Joao Pedro C. Dias <joaopedroc.dias@MacBook-Pro-de-Joao.local>
2024-04-16 11:06:43 -0300

    pushing initial project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A data science project that analysis client data and predicts the churn probability based on their purchase  profile in a telecommunications company',
+    author='João Pedro',
+    license='',
+)
